57b2dd110ff892192b32633d948394feb1ec4d87
choiyilin <yilinchoi@hotmail.com>
2025-07-16 18:12:23 -0600

    Initial Commit

---
+++ InterviewMonitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  InterviewMonitor
+//
+//  Created by WingLik Choi on 7/16/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ InterviewMonitorTests/InterviewMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewMonitorTests.swift
+//  InterviewMonitorTests
+//
+//  Created by WingLik Choi on 7/16/25.
+//
+
+import Testing
+@testable import InterviewMonitor
+
+struct InterviewMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
